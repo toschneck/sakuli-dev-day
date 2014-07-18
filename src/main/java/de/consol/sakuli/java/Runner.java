21cf15a733f3f5257236b6eced415efd4cef498c
Tobias Schneck <tobias.schneck@consol.de>
2014-07-18 18:33:54 +0200

    add first example

--- src/main/java/de/consol/sakuli/java/Runner.java
+++ src/main/java/de/consol/sakuli/java/Runner.java
@@ -3,10 +3,29 @@
  */
 package de.consol.sakuli.java;
 
+import de.consol.sakuli.datamodel.state.TestSuiteState;
+import de.consol.sakuli.starter.SakuliStarter;
+
+import java.io.FileNotFoundException;
+
 public class Runner {
 
-    public static void main() {
+    public static void main(java.lang.String[] args) {
+        int exitState = -1;
+        try {
 
-//        new SakuliStarter().main();
+
+            String testSuiteFolder = "sakuli_test_suites/example";
+            String includeFolder = "_include";
+            String sahiInstallationPath = "sahi";
+
+            final TestSuiteState resultState = SakuliStarter.runTestSuite(testSuiteFolder, includeFolder, sahiInstallationPath);
+
+            exitState = resultState.getErrorCode();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(exitState);
+        }
     }
 }

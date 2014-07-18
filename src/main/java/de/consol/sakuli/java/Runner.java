1141b9dcda8df420bc8b9c3950bfd8110d70d95a
toschneck <tobias.schneck@consol.de>
2014-07-18 16:36:32 +0200

    initial commit

---
+++ src/main/java/de/consol/sakuli/java/Runner.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tschneck on 18.07.14.
+ */
+package de.consol.sakuli.java;
+
+public class Runner {
+
+    public static void main() {
+
+//        new SakuliStarter().main();
+    }
+}

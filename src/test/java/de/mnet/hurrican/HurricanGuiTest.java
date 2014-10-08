2e2a53a9d597df9ab1c4f6edc890b27c287f4d29
toschneck <tobias.schneck@consol.de>
2014-10-08 17:03:18 +0200

    finalize hurrican test case // add empty portalweb test

--- src/test/java/de/mnet/hurrican/HurricanGuiTest.java
+++ src/test/java/de/mnet/hurrican/HurricanGuiTest.java
@@ -6,11 +6,13 @@
  */
 package de.mnet.hurrican;
 
+import de.consol.sakuli.exceptions.SakuliExceptionHandler;
 import de.consol.sakuli.javaDSL.AbstractSakuliTest;
 import de.consol.sakuli.javaDSL.TestCaseInitParameter;
 import de.consol.sakuli.javaDSL.actions.Application;
 import de.consol.sakuli.javaDSL.actions.Environment;
 import de.consol.sakuli.javaDSL.actions.Region;
+import de.consol.sakuli.loader.BeanLoader;
 import org.sikuli.script.Key;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -21,9 +23,11 @@ import java.io.File;
 /**
  * @author Tobias Schneck
  */
+@Test(groups = AbstractSakuliTest.SAKULI_TEST)
 public class HurricanGuiTest extends AbstractSakuliTest {
     private Application testling;
     private Environment env;
+    private Region hurricanRegion;
 
     @Override
     protected TestCaseInitParameter getTestCaseInitParameter() throws Throwable {
@@ -47,49 +51,78 @@ public class HurricanGuiTest extends AbstractSakuliTest {
     public void initTC() throws Throwable {
         super.initTC();
         env = new Environment();
+        hurricanRegion = startHurricanGUI();
     }
 
     @Test
-    public void hurrican1() throws Exception {
-        Region hurricanRegion = startHurricanGUI();
-
-        //search and open customer
-        hurricanRegion.find("search_customer_logo.png").click();
-        hurricanRegion.waitForImage("filter_kunde_no.png", 10)
-                .click()
-                .type(Key.HOME).type("200000407").type(Key.ENTER);
-        hurricanRegion.waitForImage("search_result_beer.png", 10)
-                .doubleClick();
-
-        //open order
-        hurricanRegion.waitForImage("auftrag_dsl_18000.png", 10)
-                .doubleClick();
-        hurricanRegion.waitForImage("auftragdaten_window.png", 10)
-                .type(Key.RIGHT + Key.RIGHT)
-                .below(300)
-                .find("tab_carrierbestellung")
-                .click();
-
-        hurricanRegion.find("elektronischer_vorgang_bt.png").click();
-        hurricanRegion.find("kue_orn_button").click();
-        hurricanRegion.waitForImage("va_erzeugen_question.png", 5)
-                .type(Key.ENTER);
-
-        hurricanRegion.find("abg_carrier_drop_down")
-                .click()
-                .below(130)
-                .find("carrier_dtag").click();
-
-        hurricanRegion.find("kundenwunschtermin").click()
-                .type("22.12.2014")
-                .type("b", Key.ALT);
-
-        hurricanRegion.waitForImage("VA_erzeugt_info", 10)
-                .type(Key.ENTER);
-
+    public void hurricanDTAG() throws Exception {
+        createWbciForCarrier("carrier_dtag");
     }
 
-    @AfterClass
+    @Test
+    public void hurricanVFDE() throws Exception {
+        createWbciForCarrier("carrier_vodafone");
+    }
+
+    private void createWbciForCarrier(String image_name_of_carrier) {
+        try {
+            //check menu is visible
+
+            new Region().exists("menu", 5);
+
+            //search and open customer
+            hurricanRegion.find("search_customer_logo.png").click();
+            hurricanRegion.waitForImage("filter_kunde_no.png", 10)
+                    .click()
+                    .type(Key.HOME).type("200000407").type(Key.ENTER);
+            hurricanRegion.waitForImage("search_result_beer.png", 10)
+                    .doubleClick();
+
+            //open order
+            hurricanRegion.waitForImage("auftrag_dsl_18000.png", 10)
+                    .doubleClick();
+            hurricanRegion.waitForImage("auftragdaten_window.png", 10)
+                    .type(Key.RIGHT + Key.RIGHT)
+                    .below(300)
+                    .find("tab_carrierbestellung")
+                    .click();
+
+            //create WBCI Vorabstimmung
+            hurricanRegion.find("elektronischer_vorgang_bt.png").click();
+            hurricanRegion.find("kue_orn_button").click();
+            hurricanRegion.waitForImage("va_erzeugen_question.png", 5)
+                    .type(Key.ENTER);
+
+            //enter carrier and kundwunschtermin
+            hurricanRegion.find("abg_carrier_drop_down")
+                    .click()
+                    .below(130)
+                    .find(image_name_of_carrier).click();
+            hurricanRegion.find("kundenwunschtermin").click()
+                    .type("22.12.2014")
+                    .type("b", Key.ALT);
+
+            //check if dialog "VA erzeugt" occurs
+            hurricanRegion.waitForImage("VA_erzeugt_info", 10)
+                    .type(Key.ENTER);
+
+            //close windows
+            hurricanRegion.type("f", Key.ALT).type("s");
+
+
+            //exception handling
+        } catch (Throwable e) {
+            SakuliExceptionHandler exceptionHandler = BeanLoader.loadBaseActionLoader().getExceptionHandler();
+            if (hurricanRegion != null) {
+                exceptionHandler.handleException(e, hurricanRegion.getRegionImpl(), false);
+            } else {
+                exceptionHandler.handleException(e);
+            }
+            throw e;
+        }
+    }
+
+    @AfterClass(alwaysRun = true)
     @Override
     public void stopTC() throws Throwable {
         try {
@@ -109,7 +142,7 @@ public class HurricanGuiTest extends AbstractSakuliTest {
         new Application("hurrican_gui\\ak-hurrican-gui.bat").open();
 
         new Region().exists("login-logo", 20)
-                .type("mnet2014!").type(Key.ENTER);
+                .typeAndDecrypt("SsgmIsfSec+9wxbORZz03Q==").type(Key.ENTER);
 
         new Region().exists("menu", 60);
         testling = new Application("M-net HURRICAN");
---
+++ src/test/java/de/mnet/hurrican/PortalWebTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2014 - M-net Telekommunikations GmbH
+ * All rights reserved.
+ * -------------------------------------------------------
+ * File created: 08.10.2014
+ */
+package de.mnet.hurrican;
+
+import de.consol.sakuli.javaDSL.AbstractSakuliTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author Tobias Schneck
+ */
+
+@Test(groups = AbstractSakuliTest.SAKULI_TEST)
+public class PortalWebTest {
+
+    @Test
+    public void testPortal() throws Exception {
+
+
+    }
+}

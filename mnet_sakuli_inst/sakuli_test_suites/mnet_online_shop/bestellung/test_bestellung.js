34d5028ab436ca67142ce60d09ea591336f3cea2
toschneck <tobias.schneck@consol.de>
2015-02-03 14:45:48 +0100

    modify test case bestellung // add pdf bestellbestaetigung validation

--- mnet_sakuli_inst/sakuli_test_suites/mnet_online_shop/bestellung/test_bestellung.js
+++ mnet_sakuli_inst/sakuli_test_suites/mnet_online_shop/bestellung/test_bestellung.js
@@ -15,14 +15,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// Pfad zu AcroRd32 in User-Env PATH hinzufügen!
-
 _dynamicInclude($includeFolder);
-var testCase = new TestCase(60, 70);
+var testCase = new TestCase(80, 100);
 var env = new Environment();
 var screen = new Region();
-var $pdfSurfix = "AGB";
+var $pdfFileName = "AGB.pdf";
+var $orderConfFileName = "Bestellbestaetigung.pdf";
+var notClosed = [];
 
 try {
     _highlight(_link("Internet & Telefon"));
@@ -33,30 +32,38 @@ try {
     env.sleep(3);
     testCase.endOfStep("select SF 100", 8);
 
-
-    _highlight(_span("Zusatzoptionen"));
-    _click(_span("Zusatzoptionen"));
-    _focus(_link("Jetzt bestellen"));
-    _highlight(_link("Jetzt bestellen"));
-    _click(_link("Jetzt bestellen"));
-    env.sleep(1);
-    _highlight(_textbox("ZipCode[1]"));
-    _setValue(_textbox("ZipCode[1]"), "80804");
-    _click(_link("80804"));
-    _highlight(_textbox("StreetWithDistrict[1]"));
-    _setValue(_textbox("StreetWithDistrict[1]"), "Düsseldorfer Str.");
-    _highlight(_link("Düsseldorfer Str."));
-    _click(_link("Düsseldorfer Str."));
-    _highlight(_textbox("HouseNumberWithExtension[1]"));
-    _setValue(_textbox("HouseNumberWithExtension[1]"), "13");
-    _highlight(_submit("Verfügbarkeit prüfen[1]"));
-    _click(_submit("Verfügbarkeit prüfen[1]"));
+    _wait(5000, _exists(_span("Surf-Flat 100")));
+    if (_exists(_span("Zusatzoptionen"))) {
+        env.logInfo("ZUSATZOPTION SITE!!!")
+        _highlight(_span("Zusatzoptionen"));
+        _click(_span("Zusatzoptionen"));
+        _focus(_link("Jetzt bestellen"));
+        _highlight(_link("Jetzt bestellen"));
+        _click(_link("Jetzt bestellen"));
+        env.sleep(1);
+        _highlight(_textbox("ZipCode[1]"));
+        _setValue(_textbox("ZipCode[1]"), "80804");
+        _click(_link("80804"));
+        _highlight(_textbox("StreetWithDistrict[1]"));
+        _setValue(_textbox("StreetWithDistrict[1]"), "Düsseldorfer Str.");
+        _highlight(_link("Düsseldorfer Str."));
+        _click(_link("Düsseldorfer Str."));
+        _highlight(_textbox("HouseNumberWithExtension[1]"));
+        _setValue(_textbox("HouseNumberWithExtension[1]"), "13");
+        _highlight(_submit("Verfügbarkeit prüfen[1]"));
+        _click(_submit("Verfügbarkeit prüfen[1]"));
+        _focus(_submit("Weiter[1]"));
+        _highlight(_submit("Weiter[1]"));
+        _click(_submit("Weiter[1]"));
+    } else {
+        env.logInfo("NEW SITE???")
+        _focus(_submit(1));
+        _highlight(_submit(1));
+        _click(_submit(1));
+    }
     env.sleep(3);
     testCase.endOfStep("check availability", 15);
 
-
-    _highlight(_submit("Weiter[1]"));
-    _click(_submit("Weiter[1]"));
     _highlight(_textbox("couponInput"));
     _setValue(_textbox("couponInput"), "SFGS25");
     env.sleep(1);
@@ -67,6 +74,7 @@ try {
     env.sleep(3);
     testCase.endOfStep("check coupon SFG25", 15);
 
+    _focus(_submit("Weiter"));
     _highlight(_submit("Weiter"));
     _click(_submit("Weiter"));
     _highlight(_select("PersonalDataForm_Salutation"));
@@ -97,25 +105,27 @@ try {
     _setValue(_textbox("PaymentForm_Iban"), "DE42720500000000066555");
     _highlight(_textbox("PaymentForm_Bic"));
     _setValue(_textbox("PaymentForm_Bic"), "AUGSDE77XXX");
+    _focus(_submit("Weiter"));
     env.sleep(3);
     _highlight(_submit("Weiter"));
     _click(_submit("Weiter"));
     env.sleep(1);
     _highlight(_submit("Weiter"));
     _click(_submit("Weiter"));
-    testCase.endOfStep("insert contact data", 15);
+    testCase.endOfStep("insert contact data", 20);
 
-    var $pdfFileLocation = _resolvePath() + $pdfSurfix + ".pdf";
+    var $pdfFileLocation = testCase.getTestCaseFolderPath() + "\\" + $pdfFileName;
     env.logInfo($pdfFileLocation);
-    _click(_link("Allgemeinen Geschäftsbedingungen"));
     _highlight(_link("Allgemeinen Geschäftsbedingungen"));
+    _click(_link("Allgemeinen Geschäftsbedingungen"));
     _saveDownloadedAs($pdfFileLocation);
     env.type("w", Key.CTRL);
     _focus(_link("Allgemeinen Geschäftsbedingungen"));
     env.sleep(3);
     openPdfFile($pdfFileLocation);
-    screen.find("agb_mnet.png").highlight();
-    closePdfReader();
+    screen.waitForImage("agb_mnet.png", 10).highlight();
+    env.type(Key.F4, Key.ALT);
+    notClosed.pop();
     testCase.endOfStep("validate AGBs", 20);
 
     _highlight(_checkbox("SummaryForm_AGB"));
@@ -127,8 +137,7 @@ try {
     _highlight(_checkbox("SummaryForm_Bankeinzug"));
     _click(_checkbox("SummaryForm_Bankeinzug"));
 
-    var regExTestSystem = /.*\.intern\.m-net\.de.*/;
-    if (testCase.getLastURL().match(regExTestSystem)) {
+    if (isTestEnvironment()) {
         _focus(_submit("Kostenpflichtig bestellen"));
         _highlight(_submit("Kostenpflichtig bestellen"));
         _click(_submit("Kostenpflichtig bestellen"));
@@ -137,10 +146,25 @@ try {
         _assert(_isVisible(_heading1("Vielen Dank für Ihre Bestellung!")));
         _highlight(_heading1("Vielen Dank für Ihre Bestellung!"));
 
+        var $conformFile = testCase.getTestCaseFolderPath() + '\\' + $orderConfFileName;
+        _highlight(_link("Dokument downloaden"))
+        _click(_link("Dokument downloaden"))
+        _saveDownloadedAs($conformFile);
+        env.type("w", Key.CTRL);
+        openPdfFile($conformFile);
+        screen.waitForImage("best_header", 10).highlight();
+        screen.find("best_vorname").highlight();
+        screen.find("best_nachname").highlight();
+        screen.find("best_tel").highlight();
+        screen.find("best_email").highlight();
+        env.type(Key.F4, Key.ALT);
+        notClosed.pop();
+        env.sleep(1);
+
     } else {
         env.logInfo("Don't send the order, in case of productive system URL: " + testCase.getLastURL());
     }
-    testCase.endOfStep("confirm and send the order", 15);
+    testCase.endOfStep("confirm and send the order", 30);
 
 } catch (e) {
     testCase.handleException(e);
@@ -148,6 +172,13 @@ try {
     testCase.saveResult();
     closePdfReader();
     //env.sleep(999);
+}
+
+
+function isTestEnvironment() {
+    var match = testCase.getLastURL().match(/.*\.intern\.m-net\.de.*/);
+    env.logInfo("testEnvironment=" + match);
+    return match;
 }
 
 /**
@@ -159,16 +190,18 @@ try {
 function openPdfFile($pdfFileLocation) {
     var appPdfReader = new Application('cmd.exe /C \"start ' + $pdfFileLocation + '"');
     appPdfReader.open();
+    notClosed.push($pdfFileLocation);
     return appPdfReader;
 }
 
-var alreadyClosed = false;
 /**
- * close the already opend PDF reader with the $pdfSurfix
+ * close the already opend PDF reader in var 'notClosed'
  */
 function closePdfReader() {
-    if (!alreadyClosed) {
-        new Application("undefined" + $pdfSurfix).focus().closeApp();
-        alreadyClosed = true;
-    }
+    notClosed.forEach(function ($file) {
+        if ($file != undefined) {
+            //remove file path and ending
+            new Application($file.replace(/.pdf/, '').replace(/.*\\/, ''), true).focus().closeApp();
+        }
+    });
 }
